b3cbc5613a42b88d4af15f82720ebc343c2d834e
rossi <sc.kwak@funzin.co.kr>
2015-08-28 05:05:21 +0000

    remove old version

--- raspberry/a.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gst-launch-1.0 -v alsasrc ! audioconvert ! queue ! audio/x-raw,rate=44100,channels=2 ! lamemp3enc target=bitrate bitrate=128 ! queue ! filesink location=a.mp3
--- raspberry/v.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-v4l2-ctl --set-fmt-video=width=1920,height=1080,pixelformat=4
-v4l2-ctl --set-parm=30
-v4l2-ctl --set-ctrl video_bitrate=1000000
-gst-launch-1.0 -e v4l2src device=/dev/video0 ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! queue ! mp4mux ! filesink location=v.mp4
