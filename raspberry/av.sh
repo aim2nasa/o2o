68333537a203bfd3bc971bb8d7ba11799fb67039
rossi <sc.kwak@funzin.co.kr>
2015-08-28 08:05:39 +0000

    change script to aware of relative path

--- raspberry/av.sh
+++ raspberry/av.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-nohup ./a.sh > audioLog&
-nohup ./v.sh > videoLog&
+
+count=$(ls | grep output | wc -l) 
+if [ "$count" -eq 0 ];then
+	mkdir output
+fi
+
+count=$(ls output | wc -l) 
+if [ "$count" -gt 0 ];then
+	rm output/*
+fi
+
+cd util
+nohup ./a.sh > ${PWD%/*}/output/audioLog 2>${PWD%/*}/output/atmp& </dev/null &
+nohup ./v.sh > ${PWD%/*}/output/videoLog 2>${PWD%/*}/output/vtmp& </dev/null &
+cd ..
--- raspberry/eos.sh
+++ raspberry/eos.sh
@@ -4,13 +4,13 @@ count=$(ps -eal|grep gst-launch-1.0|wc -l)
 echo "start,process:$count"
 
 if [ "$count" -gt 0 ];then
-	ps -eal|grep gst-launch-1.0 > plist
+	ps -eal|grep gst-launch-1.0 > output/plist
 
 	while read F S UID PID ETC
 	do
 		echo "${PID}"
 		kill -2 ${PID}
-	done < plist
+	done < output/plist
 fi
 
 while [ "$count" -ne 0 ];do
@@ -22,5 +22,9 @@ done
 count=$(ps -eal|grep gst-launch-1.0|wc -l)
 echo "end,process:$count"
 
-rm av.mp4 
-ffmpeg -i v.mp4 -i a.mp3 -acodec copy -vcodec copy av.mp4
+count=$(ls output| grep av.mp4 | wc -l)
+if [ "$count" -gt 0 ];then
+        rm output/av.mp4
+fi
+
+ffmpeg -i output/v.mp4 -i output/a.mp3 -acodec copy -vcodec copy output/av.mp4
