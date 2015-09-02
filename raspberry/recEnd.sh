cf1b186d58f5a3109fa653265b8ce3fc728c92d6
rossi <sc.kwak@funzin.co.kr>
2015-09-02 03:36:16 +0000

    simplify using shell function, note shebang changed to bash shell

--- raspberry/recEnd.sh
+++ raspberry/recEnd.sh
@@ -1,14 +1,16 @@
-#!/bin/sh
-while read pid
-do
-  #echo $pid
-  kill -2 ${pid}
-done < audio.pid
+#!/bin/bash
 
-while read pid
-do
-  #echo $pid
-  kill -2 ${pid}
-done < video.pid
+function kproc(){
+  #echo "argv: $1 $2"
+  while read pid
+  do
+    #echo $pid
+    kill -2 ${pid}
+  done < $1
+  return
+}
+
+kproc audio.pid
+kproc video.pid
 
 echo 0
