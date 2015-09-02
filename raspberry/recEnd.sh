d14d70ad802fc7a39b2450354dbbd44deff0b666
rossi <sc.kwak@funzin.co.kr>
2015-09-02 03:11:15 +0000

    kill process by sending SIGINT to process id

--- raspberry/recEnd.sh
+++ raspberry/recEnd.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+while read pid
+do
+  #echo $pid
+  kill -2 ${pid}
+done < audio.pid
 
-nohup ./eos.sh > eosLog 2>&1 </dev/null&
-
-count=$(ps -eal|grep gst-launch-1.0|wc -l)
-while [ "$count" -ne 0 ];do
-	sleep 1
-	count=$(ps -eal|grep gst-launch-1.0|wc -l)
-done
+while read pid
+do
+  #echo $pid
+  kill -2 ${pid}
+done < video.pid
 
 echo 0
