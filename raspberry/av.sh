75f6c942709bd9deddea6e179a90874d55f0c26e
rossi <sc.kwak@funzin.co.kr>
2015-09-02 02:53:02 +0000

    direct call to gst-launch from av.sh, add ignore files

--- raspberry/av.sh
+++ raspberry/av.sh
@@ -10,7 +10,12 @@ if [ "$count" -gt 0 ];then
 	rm output/*
 fi
 
+rm *.pid
+
 cd util
-nohup ./a.sh > ${PWD%/*}/output/audioLog 2>${PWD%/*}/output/atmp& </dev/null &
-nohup ./v.sh > ${PWD%/*}/output/videoLog 2>${PWD%/*}/output/vtmp& </dev/null &
+nohup gst-launch-1.0 -v alsasrc ! audioconvert ! queue ! audio/x-raw,rate=44100,channels=2 ! lamemp3enc target=bitrate bitrate=128 ! queue ! filesink location=${PWD%/*}/output/a.mp3 > ../a.log 2>&1&
+echo $! > ../audio.pid
+
+nohup gst-launch-1.0 -e v4l2src device=/dev/video0 ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! queue ! mp4mux ! filesink location=${PWD%/*}/output/v.mp4 > ../v.log 2>&1&
+echo $! > ../video.pid
 cd ..
