9894eac06f3aee9b7f3f3b61187ef23956847bea
rossi <sc.kwak@funzin.co.kr>
2015-09-08 18:38:58 +0900

    bug fix, token parsing bug after changing the format

--- rgw/merge.sh
+++ rgw/merge.sh
@@ -29,7 +29,7 @@ while :
 do
 	grep -I TOK ${source}/*
 	Token=$(grep -I TOK ${source}/*)
-	echo "Token:" $Token 	#av/300:TOK:300
+	echo "Token:" $Token 	#av/1_192.168.219.35:TOK:1 av/1_192.168.219.39:TOK:1
 	if [ ${#Token} -eq 0 ]; then
 		echo "Token is not found"
 		sleep 1 
@@ -45,12 +45,25 @@ do
 	echo "Token(s) ready:" $rpis
 
 	Token=`echo $Token | cut -d':' -f1`
-	echo "Token:" $Token	#av/300 
+	echo "Token:" $Token	#av/1_192.168.219.35 
 
 	Token=`echo $Token | cut -d'/' -f2`
-	echo "Token:" $Token	#300 
+	echo "Token:" $Token	#1_192.168.219.35
 
-	for files in `ls ${source} | grep mp3 | grep $Token`; do
+	Token=`echo $Token | cut -d'_' -f1`
+	echo "Final Token:" $Token	#1
+
+	audios=$(ls ${source} | grep mp3 | grep ${Token} | wc -l)
+	echo "mp3 files:" $audios 
+
+	if [ $audios != $nrpi ]; then
+		echo "mp3 files not match:" $audios ",expected:" $nrpi
+		rm ${source}/${Token}_*
+		echo "Tokes are deleted"
+		continue
+	fi
+
+	for files in `ls ${source} | grep mp3 | grep ${Token}`; do
 		echo "files=" $files	#300_a_192.168.219.39.mp3
 		IP=`echo $files | cut -d'_' -f3`
 		echo "IP=" $IP		#192.168.219.39.mp3
@@ -67,7 +80,7 @@ do
 		mv ${source}/${Token}_${IP}.mp4 ${target}
 		rm ${source}/${Token}_v_${IP}.mp4
 		rm ${source}/${Token}_a_${IP}.mp3
-		rm ${source}/${Token}
+		rm ${source}/${Token}_*
 	done
 	sleep 1
 done
